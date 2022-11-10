158006ae25931295cf98b23c8370773cda9826ed
odinokun <odinokun@gmail.com>
2022-11-10 23:19:41 +0100

    did about-sec

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,10 @@ $(function() {
   require('./modules/menu')();
   require('./modules/tabs')();
   require('./modules/swiper')();
+  require('./modules/scroll2ancor')();
 
   // begin phone mask
   // require('./modules/maskedinput')();
-
-  // begin Scroll2Ancor
-  // require('./modules/scroll2ancor')();
 
   // begin Popup
   // require('./modules/popup')();
--- src/js/modules/scroll2ancor.js
+++ src/js/modules/scroll2ancor.js
@@ -1,16 +1,15 @@
 module.exports = function() {
 
-  // begin scroll 2 ancore desktop menu
   $(function() {
-    $('nav a[href*=\\#]').on("click", function(e){
-       // e.preventDefault();
-       var anchor = $(this);
+    $('.scroll-down[href*=\\#]').on("click", function(e){
+       e.preventDefault();
+
+       let anchor = $(this);
        $('html, body').stop().animate({
         scrollTop: $(anchor.attr('href')).offset().top - 0 + 'px'
        }, 1000);
     });
     return false;
   });
-  // end scroll 2 ancore desktop menu
 
 };
