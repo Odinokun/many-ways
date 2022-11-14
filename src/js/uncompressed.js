f97915cd8f3b2b170eeeaa7e7bfc521ed42f7b8a
odinokun <odinokun@gmail.com>
2022-11-14 11:46:34 +0100

    did contacts page

--- src/js/uncompressed.js
+++ src/js/uncompressed.js
@@ -6,6 +6,7 @@ $(document).ready(function ($) {
     'index',
     'services',
     'service',
+    'contacts',
     ]);
 });
 
@@ -29,3 +30,41 @@ function pageWidget(pages) {
 
 
 //====== Begin Programmer code ======
+
+
+
+// let myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+// let mapOptions = {
+//   zoom: 4,
+//   center: myLatlng
+// }
+// let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//
+// let marker = new google.maps.Marker({
+//   position: myLatlng,
+//   title:"Hello World!"
+// });
+//
+// // To add the marker to the map, call setMap();
+// marker.setMap(map);
+
+
+
+
+
+
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: { lat: -33, lng: 151 },
+//   });
+//   const image =
+//     "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+//   const beachMarker = new google.maps.Marker({
+//     position: { lat: -33.89, lng: 151.274 },
+//     map,
+//     icon: image,
+//   });
+// }
+//
+// window.initMap = initMap;
